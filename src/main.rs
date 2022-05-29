9ac66816f65e504620b07183cfbc208d0ced598f
TechiePi <techie@techiepi.dev>
2022-05-29 19:52:17 +0200

    bluetooth and airplane mode hk

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::sync::{Arc, Mutex};
-use inputbot::{KeybdKey::*, MouseButton::*, MouseCursor};
+use std::thread;
+use std::time::Duration;
+use inputbot::{KeybdKey::*, MouseButton::*, MouseButton, MouseCursor};
 
 #[macro_use]
 mod macros;
@@ -7,6 +9,7 @@ mod macros;
 fn main() {
     let macro_mode = Arc::new(Mutex::new(false));
 
+    // Escape HK
     combination!(macro_mode, UpKey, NumLockKey, {
         let (x, y) = MouseCursor::pos();
 
@@ -18,14 +21,58 @@ fn main() {
         MouseCursor::move_abs(x, y);
     });
 
+    // Bluetooth HK
+    combination!(macro_mode, DownKey, UpKey, {
+        let (x, y) = MouseCursor::pos();
+
+        open_toolbar();
+
+        MouseCursor::move_abs(1725, 550);
+        LeftButton.press();
+        LeftButton.release();
+
+        thread::sleep(Duration::from_millis(300));
+
+        MouseCursor::move_abs(x, y);
+        LeftButton.press();
+        LeftButton.release();
+    });
+
+    // Airplane Mode HK
+    combination!(macro_mode, LeftKey, RightKey, {
+        let (x, y) = MouseCursor::pos();
+
+        open_toolbar();
+
+        MouseCursor::move_abs(1800, 550);
+        LeftButton.press();
+        LeftButton.release();
+
+        thread::sleep(Duration::from_millis(300));
+
+        MouseCursor::move_abs(x, y);
+        LeftButton.press();
+        LeftButton.release();
+    });
+
     if cfg!(debug_assertions) {
         let macro_mode_clone = macro_mode.clone();
-        DownKey.bind(move || {
+        RControlKey.bind(move || {
             println!("macros status -> {:?}", macro_mode_clone.lock().unwrap());
+            println!("cursor pos -> {:?}", MouseCursor::pos());
         });
     }
 
     toggle_combination!(macro_mode, LShiftKey, RShiftKey);
 
     inputbot::handle_input_events();
+}
+
+
+fn open_toolbar() {
+    MouseCursor::move_abs(1780, 1050);
+    LeftButton.press();
+    LeftButton.release();
+
+    thread::sleep(Duration::from_millis(300));
 }
